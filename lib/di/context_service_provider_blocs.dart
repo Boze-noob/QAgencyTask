e43503e7c6dc253c28e928842b5580fafbad4922
Boze-noob <bozeskoko16@gmail.com>
2022-08-13 22:19:14 +0200

    added dependency injection

---
+++ lib/di/context_service_provider_blocs.dart
@@ -0,0 +1,27 @@
+import 'package:q_agency_task/_all.dart';
+import 'package:q_agency_task/blocs/genres/_all.dart';
+
+class ContextServiceProviderBlocs extends StatelessWidget {
+  final Widget child;
+
+  const ContextServiceProviderBlocs({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      // ! Keep Blocs sorted by dependencies (only dependencies to other Blocs)
+      providers: [
+        // *
+        // * Without dependencies
+        // *
+
+        BlocProvider<GenresBloc>(
+          lazy: false,
+          create: (BuildContext context) =>
+              GenresBloc(genresRepository: context.serviceProvider.genresRepository)..add(GenresGetEvent()),
+        ),
+      ],
+      child: child,
+    );
+  }
+}
---
+++ lib/di/service_provider.dart
@@ -0,0 +1,42 @@
+import 'package:q_agency_task/_all.dart';
+
+class DevelopmentServiceProvider extends ServiceProvider {}
+
+abstract class ServiceProvider {
+  late Api api;
+  late Db db;
+  late GenresRepository genresRepository;
+
+  //Add new repositories and services here
+  //...
+
+  Future<void> init() async {
+    await initApi();
+    await initDb();
+    await initRepositories();
+  }
+
+  Future<void> initApi() async {
+    var options = BaseOptions(baseUrl: Constants.baseApiUrl, headers: {
+      "Authorization":
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOGQ3Zjc2OTQ3OTA0YTAxMTI4NmRjNzMyYzU1MjM0ZSIsInN1YiI6IjYwMzM3ODBiMTEzODZjMDAzZjk0ZmM2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.XYuIrLxvowrkevwKx-KhOiOGZ2Tn-R8tEksXq842kX4"
+    });
+    api = ApiImpl(dio: Dio(options));
+  }
+
+  Future<void> initDb() async {
+    const storage = FlutterSecureStorage();
+    db = DbImpl(storage: storage);
+  }
+
+  Future<void> initRepositories() async {
+    genresRepository = GenresRepositoryImpl(api: api, db: db);
+  }
+}
+
+Future<ServiceProvider> resolveServiceProviderFromEnvironment() async {
+  ServiceProvider serviceProvider = DevelopmentServiceProvider();
+  await serviceProvider.init();
+
+  return serviceProvider;
+}
