4c876380ae64f8bfc254318a5195748ec69072c9
Boze-noob <bozeskoko16@gmail.com>
2022-08-18 20:47:16 +0200

    refactored code

--- lib/domain/repositories/favourite_repository.dart
+++ lib/domain/repositories/favourite_repository.dart
@@ -2,6 +2,5 @@ import 'package:q_agency_task/_all.dart';
 
 abstract class FavouriteRepository {
   Future<List<MovieModel>?> getAll();
-  Future<bool> add(MovieModel movieModel);
-  Future<bool> remove(List<MovieModel> movies);
+  Future<bool> add(List<MovieModel> movies);
 }
